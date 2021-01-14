307326b41bc900ba49379f7b90c25e909f11078c
Mariatta <Mariatta@users.noreply.github.com>
2021-01-14 11:23:53 -0800

    Setting up the doc (#1)
    and ci

---
+++ docs/conf.py
@@ -0,0 +1,51 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+from datetime import datetime
+
+
+# -- Project information -----------------------------------------------------
+
+project = "Python Language Summit"
+copyright = f"2021-{datetime.now():%Y}, Python Software Foundation"
+author = "Mariatta Wijaya"
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = []
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "python_docs_theme"
+
+html_theme_options = {
+    "collapsiblesidebar": True,
+    "externalrefs": True,
+    "issues_url": "https://github.com/python/language-summit/issues",
+}
---
+++ docs/index.rst
@@ -0,0 +1,28 @@
+.. Python Language Summit documentation master file, created by
+   sphinx-quickstart on Thu Jan 14 09:40:01 2021.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Python Language Summit
+======================
+
+Issue Tracker and Discussions: https://github.com/python/language-summit
+
+For questions and suggestions about the language summit, please open an issue or
+a discussion item in the `language-summit
+<https://github.com/python/language-summit>`_ repo.
+
+Python Language Summit 2021 is scheduled over two days: **May 11 (Tuesday) and
+May 12 (Wednesday)**, it will be hosted virtually over Zoom.
+
+More details to come!
+
+**Help improve this documentation** by opening an issue or creating a pull
+request at https://github.com/python/language-summit
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+   code_of_conduct
